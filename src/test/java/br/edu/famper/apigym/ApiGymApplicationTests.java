9c95dd2a689bfd1575ff9b221ee3d5c1655179d7
Ariel Guareschi <arielguareschi@gmail.com>
2024-09-25 20:08:47 -0300

    Initial commit

---
+++ src/test/java/br/edu/famper/apigym/ApiGymApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.famper.apigym;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGymApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
